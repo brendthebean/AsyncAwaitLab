cd5e1d219744df3bbaac504b6af1f2f921ab8845
Brenden Smith <brendthebean@gmail.com>
2023-01-09 09:49:38 -0600

    clone

---
+++ index.js
@@ -0,0 +1,20 @@
+const { readFile } = require('fs').promises;
+
+const mostFrequentWord = (text) => {
+  const words = text.toLowerCase().match(/[^_\W]+/g);
+  const tally = {};
+  let mostFrequentWord = null;
+  
+  words.forEach(word => {
+    tally[word] = (tally[word] || 0) + 1 ;
+    if(!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
+      mostFrequentWord = word;
+  });
+  return mostFrequentWord;
+}
+
+const findPassword = async () => {
+  // Your code goes here
+}
+
+findPassword();
